427f8d96ec38627bab64f7717a2a0ce81db633c3
atagaev95 <predator-man@mail.ru>
2026-05-06 22:22:07 +0300

    refactor(ranks): delegate error handling to global handler

--- app/cogs/ranks.py
+++ app/cogs/ranks.py
@@ -28,32 +28,28 @@ class Ranks(commands.Cog):
 
         server_id = message.guild.id
 
-        try:
-            rank_info = await update_message_count(message.author.id, message.author.name, server_id)
+        rank_info = await update_message_count(message.author.id, message.author.name, server_id)
 
-            if rank_info["rank_up"]:
-                new_rank_description = get_rank_description(rank_info["message_count"])
-                avatar_url = (
-                    message.author.avatar.url
-                    if message.author.avatar
-                    else message.author.default_avatar.url
-                )
+        if rank_info["rank_up"]:
+            new_rank_description = get_rank_description(rank_info["message_count"])
+            avatar_url = (
+                message.author.avatar.url
+                if message.author.avatar
+                else message.author.default_avatar.url
+            )
 
-                embed, file = await em.create_rang_embed(
-                    message.author.display_name,
-                    rank_info["message_count"],
-                    new_rank_description["description"],
-                    avatar_url,
-                    server_id,
-                    message.author.id,
-                )
+            embed, file = await em.create_rang_embed(
+                message.author.display_name,
+                rank_info["message_count"],
+                new_rank_description["description"],
+                avatar_url,
+                server_id,
+                message.author.id,
+            )
 
-                await message.channel.send(
-                    f"🎉 **{message.author.mention}** повысил свой ранг!", embed=embed, file=file
-                )
-
-        except Exception as e:
-            print(f"Произошла ошибка при обновлении статистики: {e}")
+            await message.channel.send(
+                f"🎉 **{message.author.mention}** повысил свой ранг!", embed=embed, file=file
+            )
 
 
 async def setup(bot: commands.Bot) -> None:
